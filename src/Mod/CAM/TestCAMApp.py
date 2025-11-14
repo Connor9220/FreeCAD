0602236238f705307efcbc7489b01de6764e356a
sliptonic <shopinthewoods@gmail.com>
2025-11-14 11:39:31 -0600

    Merge pull request #24675 from Connor9220/PathCommandAnnotations
    CAM: Path command annotations

--- src/Mod/CAM/TestCAMApp.py
+++ src/Mod/CAM/TestCAMApp.py
@@ -29,6 +29,7 @@ from CAMTests.TestLinkingGenerator import TestGetLinkingMoves
 from CAMTests.TestPathProfile import TestPathProfile
 
 from CAMTests.TestPathAdaptive import TestPathAdaptive
+from CAMTests.TestPathCommandAnnotations import TestPathCommandAnnotations
 from CAMTests.TestPathCore import TestPathCore
 from CAMTests.TestPathDepthParams import depthTestCases
 from CAMTests.TestPathDressupDogbone import TestDressupDogbone
