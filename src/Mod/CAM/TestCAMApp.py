e4571bb658d261d6c34dfd4c77c44a766438fd42
sliptonic <shopinthewoods@gmail.com>
2026-03-30 12:46:36 -0500

    Merge pull request #27437 from petterreinholdtsen/cam-dxf-post-test
    CAM: Added trivial test of the dxf post processor.

--- src/Mod/CAM/TestCAMApp.py
+++ src/Mod/CAM/TestCAMApp.py
@@ -102,6 +102,7 @@ from CAMTests.TestPathVoronoi import TestPathVoronoi
 
 from CAMTests.TestGenericPost import TestGenericPost
 from CAMTests.TestLinuxCNCPost import TestLinuxCNCPost
+from CAMTests.TestDxfPost import TestDxfPost
 from CAMTests.TestGrblPost import TestGrblPost
 from CAMTests.TestMassoG3Post import TestMassoG3Post
 from CAMTests.TestCentroidPost import TestCentroidPost
