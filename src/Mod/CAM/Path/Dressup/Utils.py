46e24c0357b60060644d79c841904bc6989aa18f
Connor9220 <billy@ivdc.com>
2026-04-03 13:42:37 -0400

    Merge pull request #28899 from tarman3/baseOp_None
    CAM: Dressup.Utils.baseOp() - Fix for None

--- src/Mod/CAM/Path/Dressup/Utils.py
+++ src/Mod/CAM/Path/Dressup/Utils.py
@@ -37,7 +37,7 @@ def selection():
 
 def baseOp(path):
     """baseOp(path) ... return the base operation underlying the given path"""
-    if "Dressup" in path.Name:
+    if hasattr(path, "Name") and "Dressup" in path.Name:
         return baseOp(path.Base)
     return path
 
